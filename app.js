95ad250aba5a31615205317686c754d9c6abb131
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 14:09:39 -0300

    al fin, no me salia por una pinche coma

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ Ejemplo:
 const output = powerOfTwo(10);
 
 console.log(ouput); // -> 1024*/
+const powerOfTwo = function (num){//num será el exponente
+    let result=1;
+    
+    for(let i=0; i< num; i++){
+    result *= 2;
+    }
+    return result;
+};
+console.log(powerOfTwo(10));// cuando num = 0 retorna 1, cuando num=10 retorna 1024,
